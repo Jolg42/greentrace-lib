7e76f7197a9cce1a20cd589c3cc91047afd62680
Chris Adams <chris@productscience.co.uk>
2019-07-13 15:55:02 +0200

    Fix the broken example to log geoJSON to STDOUT

--- src/check-google.js
+++ src/check-google.js
@@ -5,10 +5,9 @@ const debug = debugLib("tgwf:greenTrace:cli")
 // sample usage.
 const main = async () => {
   const hops = await GreenTrace.trace("google.com")
-  debug(hops)
   const hopsAsGeoJSON = GreenTrace.toGeoJSON(hops)
-  debug())
-  console.log(JSON.stringify(hopsAsGeoJSON)
+  debug(hops)
+  console.log(JSON.stringify(hopsAsGeoJSON))
 }
 
 main()
